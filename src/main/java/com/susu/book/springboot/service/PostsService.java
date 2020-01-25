d71f2af09b57b656120784af76d89d089acdc9a7
sungwon <zhtmr123@gmail.com>
2020-01-25 14:47:45 +0900

    2020.1.25

--- src/main/java/com/susu/book/springboot/service/PostsService.java
+++ src/main/java/com/susu/book/springboot/service/PostsService.java
@@ -19,9 +19,30 @@ public class PostsService {
 
     }
 
+    /*  [ 람다식 안쓴 코드 ]
+
+    orElseThrow(new Supplier<IllegalArgumentException>() {
+        @Override
+        public IllegalArgumentException get() {
+            return new IlleagalArgumentException("해당 사용자가 없습니다. id="+id);
+        }
+
+        [ 람다식 사용 ]
+
+        orElseThrow(() -> { return new IllegalArgumentException("해당 사용자가 없습니다. id="+id); });
+
+        [ 람다식의 함수 몸체에 실행문이 한개이고,
+        그 실행문이 return문일 경우 중괄호, return 생략가능 ]
+
+        orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" +id));
+
+        [ 람다식의 사용 목적 ]
+        람다식은 결국 로컬 익명 구현객체를 생성하게 되지만, 이 람다식의 사용목적은 인터페이스가 가지고 있는
+        메소드를 간편하게 즉흥적으로 구현해서 사용하는 것이 목적이다. (함수형 프로그래밍, 병렬처리와 이벤트 지향 프로그래밍)
+     */
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
