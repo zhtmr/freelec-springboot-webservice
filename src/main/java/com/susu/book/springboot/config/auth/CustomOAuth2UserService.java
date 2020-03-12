611e2ad2833232cd9637edc83d18beb03da1a6c8
sungwon <zhtmr123@gmail.com>
2020-03-12 19:14:04 +0900

    2020.3.12 config.auth

--- src/main/java/com/susu/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/susu/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -1,6 +1,7 @@
 package com.susu.book.springboot.config.auth;
 
 import com.susu.book.springboot.config.auth.dto.OAuthAttributes;
+import com.susu.book.springboot.config.auth.dto.SessionUser;
 import com.susu.book.springboot.domain.user.User;
 import com.susu.book.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
