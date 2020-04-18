ddf274185c4a38dd854bcc83dddd21721c1baf39
sungwon <48509269+zhtmr@users.noreply.github.com>
2020-04-18 18:19:15 +0900

    2020.4.18 appspec.yml 띄어쓰기수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-#PROJECT_NAME=freelec-springboot-webservice
+# shellcheck disable=SC2034
+PROJECT_NAME=freelec-springboot-webservice
 
 echo "> Build 파일 복사"
 
@@ -14,11 +15,11 @@ CURRENT_PID=$(pgrep -fl freelec-springboot-webservice | grep jar | awk '{print $
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
