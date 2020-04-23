1ec2f6e3425dc0d69b9e4312dba9782e7b3f844d
sungwon <48509269+zhtmr@users.noreply.github.com>
2020-04-23 23:28:05 +0900

    2020.4.23 ProfileController - 삼항연산자, stream(), 람다식과 메소드레퍼런스 차이점 봐야됨

---
+++ src/main/java/com/susu/book/springboot/web/ProfileController.java
@@ -0,0 +1,59 @@
+package com.susu.book.springboot.web;
+
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Nginx 에서 8081, 8082 판단 기준
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+  private final Environment env;
+
+  @GetMapping("/profile")
+  public String profile(){
+    // real, oauth, real-db 등의 현재 실행중인 프로필(.properties 파일)
+    List<String> profiles = Arrays.asList(env.getActiveProfiles()); // asList : 배열을 List 형태로 가져옴. 원본 배열을 수정해도 List 바뀌고, 그 반대도 마찬가지.
+    List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+    // 삼항연산자
+    //  String defaultProfile;
+    //    if (profiles.isEmpty()) {
+    //      defaultProfile = "default";
+    //    } else {
+    //      defaultProfile = profiles.get(0);
+    //    }
+    String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+    // ---- Stream API chain with loop 대신 아래처럼 사용가능
+    //  for (String profile : profiles) {
+    //      if (realProfiles.contains(profile)) {
+    //        return profile;
+    //      }
+    //    }
+    //    return defaultProfile;
+
+    // Stream은 자바 8부터 추가된 기능으로 "컬렉션, 배열등의 저장 요소를 하나씩 참조하며 함수형 인터페이스(람다식)를 적용하며 반복적으로 처리할 수 있도록 해주는 기능"이다.
+    return  profiles.stream()
+        .filter(realProfiles::contains)
+        .findAny()
+        .orElse(defaultProfile);
+
+//    List<String> names = Arrays.asList("jeong", "pro", "jdk", "java");
+//// 기존의 코딩 방식
+//    long count = 0;
+//    for (String name : names) {
+//      if (name.contains("o")) {
+//        count++;
+//      }
+//    }
+//    System.out.println("Count : " + count); // 2
+//
+//// 스트림 이용한 방식
+//    count = 0;
+//    count = names.stream().filter(x -> x.contains("o")).count();
+//    System.out.println("Count : " + count); // 2
+  }
+}
