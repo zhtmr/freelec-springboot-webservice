32f8f7cda1a64aaf1860d2ea0843bff51415f105
sungwon <48509269+zhtmr@users.noreply.github.com>
2020-04-28 23:01:32 +0900

    4.28 stop.sh IDLE_PID -> IDLE_PORT

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH) # 현재 stop.sh가 속해 있는 경로
-source ${ABSDIR}/profile.sh # import profile.sh
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
 
-echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLE_PID})
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
