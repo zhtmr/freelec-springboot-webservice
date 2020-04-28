b8548c78a70a24b94516ab311e4a2208755f1f3d
sungwon <48509269+zhtmr@users.noreply.github.com>
2020-04-28 13:53:32 +0900

    4.28

--- src/test/java/com/susu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/susu/book/springboot/web/IndexControllerTest.java
@@ -1,14 +1,14 @@
 package com.susu.book.springboot.web;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -23,6 +23,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("정보쉣");
     }
 }
