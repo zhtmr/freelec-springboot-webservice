43d4a3801fe7cc0fba26d68f383a9a518a9eea3f
sungwon <48509269+zhtmr@users.noreply.github.com>
2020-04-28 17:32:58 +0900

    4.28

--- src/test/java/com/susu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/susu/book/springboot/web/IndexControllerTest.java
@@ -23,6 +23,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("정보쉣");
+        assertThat(body).contains("스프링부트 게시판");
     }
 }
