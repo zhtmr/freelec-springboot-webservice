0e4237577da1fdcc545c536d9addf5a72eec9246
sungwon <48509269+zhtmr@users.noreply.github.com>
2020-04-28 23:31:32 +0900

    4.28 health.sh profile 다음 띄우기

--- scripts/health.sh
+++ scripts/health.sh
@@ -9,7 +9,7 @@ IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -s http://localhost:$IDLE_PORT/profile"
+echo "> curl -s http://localhost:$IDLE_PORT/profile "
 sleep 10
 
 for RETRY_COUNT in {1..10}
@@ -19,17 +19,17 @@ do
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
-    echo "> Health check 성공"
-    switch_proxy
-    break
+      echo "> Health check 성공"
+      switch_proxy
+      break
   else
-    echo "> Health check 의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health check: ${RESPONSE}"
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
- then
-    echo "> Health check 실패"
+  then
+    echo "> Health check 실패. "
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
     exit 1
   fi
